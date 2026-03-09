bf8218efdcbee9831a353b4fe61c389556ba3522
afzalbare <afzalbare26@gmail.com>
2026-03-09 08:37:55 +0600

    js variable

---
+++ main.js
@@ -0,0 +1,35 @@
+// Js Variable
+
+// A JavaScript variable is a container used to store data so that it can be used later in the program.
+
+/*
+?Simple definition:
+
+Variable = a place to store values.
+*/
+
+// TODO: use let
+
+let name = "afzal"; // variable
+let age = 25; // value
+console.log(name);
+// TODO: use  const
+const pi = 3.14;
+//! If you use const, the value cannot be changed later.
+
+const pop = 1.25;
+//pop = 2; // TypeError: Assignment to constant variable
+
+// TODO: use var
+
+var city = "Dhaka";
+
+/*
+ !let vs const vs var 
+
+keyword ==	can change == modern ==	recommended
+var         	yes	       old	     ❌ avoid
+let	            yes	       yes	      ✅ use
+const	         no	       yes	       ✅ best
+
+*/
