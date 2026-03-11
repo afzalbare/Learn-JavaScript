edb78e4c6a32b8afb60cf10ea7ad74a14588960b
afzalbare <afzalbare26@gmail.com>
2026-03-11 08:06:42 +0600

    problem solving

--- main.js
+++ main.js
@@ -33,3 +33,33 @@ let	            yes	       yes	      ✅ use
 const	         no	       yes	       ✅ best
 
 */
+
+/*
+
+!Practice Problems
+
+*1. Keep your birth year in a variable.
+const birthYear = 1998;
+
+*2. Keep the number of brothers and sisters you have in a variable.
+const brothersAndSisters = 5;
+
+*3. Declare a variable called students with the number of students in your class.
+const students = 30;
+
+*4. Declare a variable called marks with the number of marks you got in the last exam.
+const marks = 85;
+
+*5. Declare a variable called rooms with the number of rooms in your house.
+const rooms = 4;
+
+*6. Keep the number of hours you study per day in a variable.
+const studyHours = 3;
+
+*7. Add all the digits in your phone number and keep the sum in a variable called total.
+const phoneNumber = 0+1+8+9+3+7+2+4+8+6+7;
+const total = phoneNumber;
+console.log(total);
+
+
+*/
