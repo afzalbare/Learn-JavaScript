27610d0aaad816b7da40ea92024ebf992c2fecc6
afzalbare <afzalbare26@gmail.com>
2026-03-16 10:41:38 +0600

    Primitive Data Types Add

---
+++ js-data-type.js
@@ -0,0 +1,27 @@
+/* 
+!There are two main types of data types in JavaScript:
+? 1. Primitive Data Types (hold a single value)
+? 2. Non-Primitive (Reference) Data Types (hold multiple or complex values) 
+
+Primitive Data Types:
+1. String: Represents textual data, enclosed in single or double quotes. 
+TODO:  Example: let name = "Rahim";
+
+2. Number: Represents both integer and floating-point numbers.
+TODO: Example: let score = 95;
+
+3. Boolean: Represents true or false values. 
+TODO: Example: let isStudent = true;
+
+4. Null: Represents the absence of a value. 
+TODO: Example: let emptyValue = null;
+
+5. Undefined: Represents a variable that has not been assigned a value yet. 
+TODO: Example: let x;
+
+6. Symbol: Represents a unique identifier, often used for object properties. 
+TODO: Example: let id = Symbol("id");
+
+BigInt: Represents integers with arbitrary precision, allowing for very large numbers. 
+TODO: Example: let bigNumber = 123456789012345678901234567890n;
+*/
